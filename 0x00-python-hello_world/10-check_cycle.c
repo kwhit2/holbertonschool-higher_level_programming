bd75ecae1bbfc9f68163a804f975917338d0f675
kwhit2 <kfw2@outlook.com>
2020-12-07 18:53:15 +0000

    Preimptive Task 10 Upload

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* allowed functions are write, printf, putchar, puts, malloc, free
+*
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+
+
+}
