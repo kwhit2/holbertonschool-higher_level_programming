bd5a75ec0cccb976bc944ff22989b1afe7a4ca44
kwhit2 <kfw2@outlook.com>
2020-12-08 18:55:33 +0000

    Oops

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):  # for numbers 0-98
-        print("{} = {}".format(i, hex(i)))  # print "num = hex of num"
+    print("{} = {}".format(i, hex(i)))  # print "num = hex of num"
