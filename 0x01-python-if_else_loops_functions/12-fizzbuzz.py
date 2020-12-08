69e67a48cbbd3c039769156da35b0aa6eb95e71c
kwhit2 <kfw2@outlook.com>
2020-12-08 17:08:00 +0000

    Added Comments

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
+    for i in range(1, 101):  # for numbers 1-100
         if i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif i == 99:
+        elif i == 99:  # to get \n at end
             print(i)
         else:
-            print(i, end=" ")
+            print(i, end=" ")  # for numbers not divisible by 3 0r 5
