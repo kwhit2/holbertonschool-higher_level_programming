db78a80b5792309fdea2cd222615bcc648ec79e3
kwhit2 <kfw2@outlook.com>
2020-12-08 18:45:06 +0000

    Fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-import string  # import string library function
-result = string.ascii_lowercase  # ascii_lowercase if a constant of string mod
-print(result, end="")  # printing the value/lowercase alphabet
+for char in range(97, 123):
+    print("{}".format(chr(char)), end="")
