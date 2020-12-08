f26fb5d928f31adc2b88697c1cb8c9688d8b2502
kwhit2 <kfw2@outlook.com>
2020-12-08 19:45:58 +0000

    Advanced Task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in reversed(range(ord("a"), ord("z") + 1)):  # +1 for range to include z
+    if (i % 2) != 0:  # if it's odd
+        i = i - 32  # make it upper
+    print("{:c}".format(i), end="")  # :c to print as char
