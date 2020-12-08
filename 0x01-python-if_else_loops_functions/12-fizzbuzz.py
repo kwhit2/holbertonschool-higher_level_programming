2b03b81e591f09b9fb7d391898a855e1735689a2
kwhit2 <kfw2@outlook.com>
2020-12-08 19:12:44 +0000

    Removed Comments for checker

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):  # for numbers 1-100
+    for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif i == 99:  # to get \n at end
+        elif i == 99:
             print(i)
         else:
-            print(i, end=" ")  # for numbers not divisible by 3 0r 5
+            print(i, end=" ")
