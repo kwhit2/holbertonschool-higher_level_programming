0eee9b710fa96d4afe273b5a59573895f96a7538
kwhit2 <kfw2@outlook.com>
2020-12-09 14:13:10 +0000

    Trying new format agein

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return (if n >= 0 str[0:n] + str[n+1:] else str)
+    #  return (if n >= 0 str[0:n] + str[n+1:] else str)
     #  str[0:n] get string up until n
     #  str[n+1] adds/concatenates string after n
     #  if n is negative print the string
+    if n >= 0:
+        return str[0:n] + str[n+1:]
+    else:
+        return str
