3d90dfbb702c8003cde526d3986296e77852e0e7
kwhit2 <kfw2@outlook.com>
2020-12-09 14:16:02 +0000

    Final Format & Comments

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    #  return (if n >= 0 str[0:n] + str[n+1:] else str)
+    #  return (str[0:n] + str[n+1:] if n >= 0 else str)
     #  str[0:n] get string up until n
     #  str[n+1] adds/concatenates string after n
     #  if n is negative print the string
