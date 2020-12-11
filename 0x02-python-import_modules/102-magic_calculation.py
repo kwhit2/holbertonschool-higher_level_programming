ab8c0a49d6ce00f413bad5cf13adf44fb24d25c9
kwhit2 <kfw2@outlook.com>
2020-12-11 01:16:25 +0000

    Fix

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from magic_calculations import add, sub
-if __name__ == '__main__':
-    if a < b
-    c = add(a, b)
-    for i in range(4, 6):
-            c = add(c, i)
-    return c
-else:
-    return (a - b)
+def magic_calculations(a. b):
+    from magic_calculations import add, sub
+    if __name__ == '__main__':
+        if a < b
+        c = add(a, b)
+        for i in range(4, 6):
+                c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
