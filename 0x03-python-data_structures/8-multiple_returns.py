5953ed6cdbea74fa559464b5e1ca3029165c3dba
kwhit2 <kfw2@outlook.com>
2020-12-15 19:30:54 +0000

    Fix and Comments

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,3 +4,10 @@ def multiple_returns(sentence):
     first = sentence[0] if total > 0 else "None"
     new = total, first
     return (new)
+
+#  Below also works
+#  def multiple_returns(sentence):
+#        if sentence == "" or sentence is None:
+#           return (0, None)
+#        else:
+#            return (len(sentence), sentence[0])
