029034be2da15e20683656169c03fd1c8a3904b8
kwhit2 <kfw2@outlook.com>
2020-12-16 19:23:12 +0000

    Task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -21,5 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int pal_meat(listint_t **left, listint_t *right);
 
 #endif /* LISTS_H */
