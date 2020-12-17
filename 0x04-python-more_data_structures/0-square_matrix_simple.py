92ef2a7778573666ca54ae5d5ff59cfbff68e9d5
kwhit2 <kfw2@outlook.com>
2020-12-17 17:34:17 +0000

    Update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,3 +2,9 @@
 def square_matrix_simple(matrix=[]):
     if matrix:
         return [list(map(lambda x: x**2, x)) for x in matrix]
+
+#  Did not work
+#  newmatrix = matrix[:]
+#  for x in newmatrix:
+#  newmatrix = [y*y for y in x]
+#  return newmatrix
