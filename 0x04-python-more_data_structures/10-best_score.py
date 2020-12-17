a627be674a5c23e9500a21f76a9ea286c897a882
kwhit2 <kfw2@outlook.com>
2020-12-17 18:31:57 +0000

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if not a_dictionary:
+        return
+    max_key = max(a_dictionary, key=lambda x: a_dictionary[x])
+    return (max_key)
 
-#  Same as above, diff format. Correct Output/missing 2 checks
+#  2 code below are same but diff format. Correct Output/missing 2 checks
+#  return (max(a_dictionary) if a_dictionary else None)
+#  ----------------------------------------------------
 #  if not a_dictionary:
 #      return None
 #  return (max(a_dictionary))
