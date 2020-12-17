3faab3bcd564ad3fbd0d049c215743559d656efa
kwhit2 <kfw2@outlook.com>
2020-12-17 17:26:21 +0000

    Task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+#  if the key doesn't exist it will be created
+#  if the key does exist it will be overwritten/replaced
