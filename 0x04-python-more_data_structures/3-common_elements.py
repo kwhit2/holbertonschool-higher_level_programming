860506657d9ce83b3ce7ad0633618ff848f75dc2
kwhit2 <kfw2@outlook.com>
2020-12-18 19:31:48 +0000

    Update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,7 +7,7 @@ def common_elements(set_1, set_2):
             result.append(element)  # add matching chars (C) and
     return result                   # adds/appends to result which is returned
 
-#  Below also works in 1 line of code
-#  return set_1 && set_2
+#  Below also works in 1 line of code: not positive which and operator works
+#  return set_1 && set_2 OR return set_1 and set_2
 #  Below also works in 1 line of code
 #  return set_1.intersection(set_2)
