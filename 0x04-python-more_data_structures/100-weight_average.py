897eb5064b309f4697ea1db577bbfaa76b3ac594
kwhit2 <kfw2@outlook.com>
2020-12-18 16:28:48 +0000

    Advanced Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    for x in range(len(my_list)):
-        return x
+
+    if not my_list:
+        return 0
+    a = 0
+    b = 0
+    for weight in my_list:
+        a += (weight[0] * weight[1])
+        b += weight[1]
+    return (a/b)
