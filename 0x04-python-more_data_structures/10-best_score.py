be3d8ca1e9733d6d2bb45eeecaff9b5eb928fc7a
kwhit2 <kfw2@outlook.com>
2020-12-18 19:11:05 +0000

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,9 +5,8 @@ def best_score(a_dictionary):
     max_key = max(a_dictionary, key=lambda x: a_dictionary[x])
     return (max_key)
 
-#  2 code below are same but diff format. Correct Output/missing 2 checks
-#  return (max(a_dictionary) if a_dictionary else None)
-#  ----------------------------------------------------
+#  Another way that works WITHOUT using lambda
 #  if not a_dictionary:
-#      return None
-#  return (max(a_dictionary))
+#    return
+#  max_key = max(a_dictionary, key=a_dictionary.get)
+#    return max_key
