f5db3637e8092ee22106cf623e73abb1f49952c6
kwhit2 <kfw2@outlook.com>
2020-12-18 14:10:10 +0000

    Comments

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
 
-    result = []
-    for element in set_1:
-        if element in set_2:
-            result.append(element)
-    return result
+    result = []  # empty list
+    for element in set_1:  # checks all chars of set 1
+        if element in set_2:  # checks all chars of set 2
+            result.append(element)  # add matching chars (C) and
+    return result                   # adds/appends to result which is returned
 
 #    if (set_1 & set_2):
 #        print(set_1 & set_2)
