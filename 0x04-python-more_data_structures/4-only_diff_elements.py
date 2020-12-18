975a6c77ef87b1d10f930a16ce38d78884c4208a
kwhit2 <kfw2@outlook.com>
2020-12-18 14:12:23 +0000

    Comments

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
 
-    result = []
-    for element in set_1:
-        if element not in set_2:
+    result = []  # empty list
+    for element in set_1:  # same as previous task but opposite
+        if element not in set_2:  # make sure elements of set1 are not in set2
             result.append(element)
     for element in set_2:
         if element not in set_1:
