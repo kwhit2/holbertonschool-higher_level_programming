248d80bd0fae76f7e83c2d26269799bd5d257ae6
kwhit2 <kfw2@outlook.com>
2021-01-05 20:09:25 +0000

    Format Update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """ Square class
-    """
+    """ Square class """
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """ Square class
-    """
+    """ Square class """
     def __init__(self, size=0):
         """__init__ method initialization
         Args:
