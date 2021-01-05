4d7e34ed2bd15be05cb46cba57b01efdbec16c6f
kwhit2 <kfw2@outlook.com>
2021-01-05 22:21:31 +0000

    Updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+""" This module contains a class Square """
+
+
 class Square:
-    """ Square class """
+    """ Square class. Represents a square
+
+    Attributes:
+        __size (int): size of 1 side of square
+    """
     def __init__(self, size):
+        """ Initializes the square
+
+        Args:
+            size (int): size of 1 side of square
+
+        Return: None
+        """
         self.__size = size
