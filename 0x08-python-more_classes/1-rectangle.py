0e548ed80c63274b323b84643d9b252e46f379c4
kwhit2 <kfw2@outlook.com>
2021-01-12 00:11:51 +0000

    Adjusted Private Instances

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             width: integer
             height: integer
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
