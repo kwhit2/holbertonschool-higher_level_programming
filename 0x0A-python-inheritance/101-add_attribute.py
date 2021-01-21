66718042de9ba46263eabd8fa26b9b2dc1be89e0
kwhit2 <kfw2@outlook.com>
2021-01-21 17:16:39 +0000

    Update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-
-
-class MyClass():
-    pass
+""" This module contains a function that
+    adds a new attribute to an object if it’s possible """
 
 
 def add_attribute(obj, name, value):
     """ is_same_class method:
         args: obj - object to create new attribute for
-              name - name of attribute
-              value - value (string)
+            name - name of attribute
+            value - value (string)
         raises: TypeError: if the object can't have new attribute
         return:
             None
     """
-    if obj is None or name is None or value is None:
+    if '__dict__' not in dir(obj):
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, name, value)
