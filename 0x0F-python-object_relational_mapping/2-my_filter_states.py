d99c46dd27d2e127fa86030ccfc3a3bcb20c9a85
kwhit2 <kfw2@outlook.com>
2021-03-25 18:38:15 +0000

    needed to   to the query to be able to compile. Pep8 lied.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import MySQLdb
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY
-                id ASC".format(argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name='{}' ORDER BY
+                id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
