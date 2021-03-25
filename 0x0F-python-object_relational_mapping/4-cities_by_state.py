bca97f87d86a227afa6f365168a77f8cb92522f0
kwhit2 <kfw2@outlook.com>
2021-03-25 19:09:00 +0000

    0x0E-9-cities_by_state_join.sql:Docstring Update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) from...
-    ...the database hbtn_0e_0_usa """
+""" script that lists all cities from the database hbtn_0e_4_usa """
 
 
 if __name__ == "__main__":
