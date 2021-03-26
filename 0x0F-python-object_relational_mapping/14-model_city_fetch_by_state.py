0a9f6ecc4bab329cdf4bd94e4b4360688e71dfb3
kwhit2 <kfw2@outlook.com>
2021-03-26 20:03:37 +0000

    Foreign key was tricky and needed to add a join to each city wasn't in every state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  script that prints all City objects from the database hbtn_0e_14_usa """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state, city in session.query(State, City).join(City).order_by(
+            City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
