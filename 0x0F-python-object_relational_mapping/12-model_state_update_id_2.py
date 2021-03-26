d2bd749fd1edf134d4d1c5ac92438b5878b4c985
kwhit2 <kfw2@outlook.com>
2021-03-26 14:13:30 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that lists all State objects that contain the letter a from the...
+    ...database hbtn_0e_6_usa """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           ("root", "root", argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.id.like(2)):
+        state.name = 'New Mexico'
+    session.commit()
+    session.close()
