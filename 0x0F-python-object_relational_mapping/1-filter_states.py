de0ccfe46b81ee7012432fa9c88e2061f702f7b9
kwhit2 <kfw2@outlook.com>
2021-03-27 15:44:18 +0000

    Latin is default sql language and make it case sensitive?

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     import MySQLdb
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states WHERE name like 'N%'
+                COLLATE latin1_general_cs ORDER BY id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
