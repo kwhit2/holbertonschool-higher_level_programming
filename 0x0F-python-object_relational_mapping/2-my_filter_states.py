6d993e46ff96bd537dac8df12285f2b222311ba5
kwhit2 <kfw2@outlook.com>
2021-03-27 18:30:14 +0000

    Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states WHERE name='{}'
-                COLLATE latin1_general_cs ORDER BY
+                COLLATE Latin1_General_CS ORDER BY
                 id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
