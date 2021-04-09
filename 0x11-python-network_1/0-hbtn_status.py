53f80a5c728d59e100de6b5d654d17f5b700d0f5
kwhit2 <kfw2@outlook.com>
2021-04-09 15:44:19 +0000

    Task 0-2 are running with output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-# a Python script that fetches https://intranet.hbtn.io/status
+""" a Python script that fetches https://intranet.hbtn.io/status
+    using urllib """
+
+
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    utf8 = html.decode('utf-8')
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(utf8))
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL and an email, sends a POST request to..
+    ..the passed URL with the email as a parameter, and displays the body of...
+    ...the response (decoded in utf-8) """
+
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+url = argv[1]
+email = argv[2]
+
+params = urllib.parse.urlencode({'email': email})
+params = params.encode('ascii')
+request = urllib.request.Request(url, params)
+
+with urllib.request.urlopen(request) as response:
+    content = response.read()
+    utf8 = content.decode('utf-8')
+print(utf8)
