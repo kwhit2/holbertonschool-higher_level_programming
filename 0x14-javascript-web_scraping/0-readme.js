d05cc8bd6c53479711e2613799c598eae52796aa
kwhit2 <kfw2@outlook.com>
2021-05-04 16:50:12 +0000

    Correct output

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// a script that reads and prints the content of a file
+const fs = require('fs');
+const process = require('process');
+
+const args = process.argv.slice(2);
+
+fs.readFile(args[0], (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
