c499a970f1ac7c40af76450d7b35550398076465
kwhit2 <kfw2@outlook.com>
2021-05-12 18:22:43 +0000

    Working in browser

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that updates the text color of the <header> element to
+// red (#FF0000):
+// must use document.querySelector to select the HTML tag
+// Note: Your script must be imported from the <head> tag, not at the end of the HTML
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style = 'color: red;';
+});
